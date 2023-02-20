8c96f5acb636f9cd344b187f8e40d9a955f8d977
Waminms <67516571+Waminms@users.noreply.github.com>
2023-02-20 22:42:46 +0200

    Predicate improvements, iFinal removed

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,16 @@ public class Main {
         List<String> list1 = new ArrayList<>(List.of("head", "black", "final", "art", "orange", "test", "hot"));
         System.out.println("initial list: " + list1);
 
-        for (int i = 0; i < checkList.size(); i++) {
-            int iFinal = i;
-            Predicate<String> prs = (a) -> a.startsWith(checkList.get(iFinal));
-            list1.removeIf(prs);
-        }
+        Predicate<String> prs = (a) -> {
+            for (String s : checkList) {
+                if (a.startsWith(s)) {
+                    return true;
+                }
+            }
+            return false;
+        };
 
+        list1.removeIf(prs);
         System.out.println("transformed list: " + list1);
     }
 }
